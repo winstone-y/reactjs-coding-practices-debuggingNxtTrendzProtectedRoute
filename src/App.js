873333b8cb70229f439209c560013b31468e9481
Winstone <winstonwork57@gmail.com>
2023-06-22 10:14:04 +0000

    debuggingNxtTrendzProtectedRoute

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import {Switch, Route} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Products from './components/Products'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+import ProtectedRoute from './components/ProtectedRoute'
+import Header from './components/Header'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <Header />
+    <Switch>
+      <Route exact path="/login" component={LoginForm} />
+      <ProtectedRoute exact path="/" component={Home} />
+      <ProtectedRoute exact path="/products" component={Products} />
+      <ProtectedRoute exact path="/cart" component={Cart} />
+      <Route component={NotFound} />
+    </Switch>
+  </>
+)
+
+export default App
